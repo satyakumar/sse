cdb0416a7f52ee3288c4462faae8dc1ffbd4d381
APTTUS\skotipalli <skotipalli@apttus.com>
2020-04-18 17:12:33 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Buttons-Grid';
+  private _gridAndActions = Array.from({length: 4}, (_, value) => value + 1);
+  grids: Array<Number> = this._gridAndActions; // Used to show components based on defined Array
+  toggleGrids: Set<Number> = new Set(this._gridAndActions); // Used to toggle components by playing around the Array
+  gridActions: Array<Number> = this._gridAndActions; // Used to show the Buttons on Child components
+
+  updateGridSequence(sequence) { 
+    this.toggleGrids.has(sequence) ? this.toggleGrids.delete(sequence) : this.toggleGrids.add(sequence);
+  }
+  isGridShow(gridSequence) {
+    return this.toggleGrids.has(gridSequence);
+  }
 }
---
+++ src/app/grid/grid.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+//import { EventEmitter } from 'protractor';
+
+@Component({
+  selector: 'app-grid',
+  templateUrl: './grid.component.html',
+  styleUrls: ['./grid.component.css']
+})
+export class GridComponent implements OnInit {
+  @Input() actions: Number;
+  @Input() gridSequence: Number;
+  @Output() onClick = new EventEmitter();
+  constructor() { }
+
+  ngOnInit() {
+    console.log(this);
+  }
+
+  clickMe(actionSequence) {
+    this.onClick.emit(actionSequence);
+  }
+
+}
